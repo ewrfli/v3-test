23522a41c4a376759317907fac24947729809814
ewrfli <q304062@163.com>
2022-01-14 17:58:26 +0800

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-
+import viteCompression from 'vite-plugin-compression'
+//@ts-ignore
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', //打包路径
-  plugins: [vue()],
+  base: './build', //打包路径
+  plugins: [
+    vue(),
+    // gzip压缩 生产环境生成 .gz 文件
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  //启动服务配置
   server: {
     host: '0.0.0.0',
     port: 3001,
@@ -13,6 +25,7 @@ export default defineConfig({
     https: false,
     proxy: {}
   },
+  // 生产环境打包配置 去除 console debugger
   build:{
     terserOptions: {
         compress: {
